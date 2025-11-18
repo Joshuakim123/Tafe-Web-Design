cb914f798c71c48bcbf88c33ffa7341ebd875be9
joshua <joshua.kim10@studytafensw.edu.au>
2025-11-18 12:12:56 +1100

    assessment part 3

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 function onPageLoad () {
-    // find the nav button and refer to it as a "button"
     const button = document.querySelector('#menu-button')
-    // find the navigation and reger to it as "menu"
     const menu = document.querySelector('#main')
-    // added a sound for click on the button and execute function when clicked
     button.addEventListener("click", function() {
-        // console.log("clicked!")
         if( menu.classList.contains("open")){
             menu.classList.remove("open")
         }
